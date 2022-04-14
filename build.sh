da654425b3a36160a252e68c88422439076019f7
Darwish98 <66258375+Darwish98@users.noreply.github.com>
2022-04-14 11:07:02 +0200

    updates

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ if [ -z $1 ];
 fi
   
 NOTEBOOKS_PATH=$1
-nvidia-docker build --tag tstoyanov/tf-openailab-gpu ./
-nvidia-docker run -d --name jupyter -v $NOTEBOOKS_PATH:/notebooks -e NB_UID=1000 -e NB_GID=1000 -e VNC_SERVER_PASSWORD=password -p 8888:8888 -p 6006:6006 -p 5900:5900  tstoyanov/tf-openailab-gpu 
+nvidia-docker build --tag mohdar/tf-gpu ./
+nvidia-docker run -d --name jupyter -v $NOTEBOOKS_PATH:/notebooks -e NB_UID=1000 -e NB_GID=1000 -e VNC_SERVER_PASSWORD=password -p 8888:8888 -p 6006:6006 -p 5900:5900  mohdar/tf-gpu 
 echo "build succeeded, wait a bit for X to start, then getting the jupyter notebook URL with token"
 sleep 3
 docker exec -it jupyter bash -c "jupyter notebook list"
